dd2667ab8355f4756e89025cd99c0205a0c5132e
atikmahmud482 <atikmahmud482@gmail.com>
2025-04-22 21:55:57 +0600

    redux file upload

---
+++ src/features/products/productsAPI.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const response = await axios.get(
+      "https://admin.refabry.com/api/all/product/get"
+    );
+    return response.data;
+  }
+);
---
+++ src/features/products/productsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  status: "idle", // 'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+  selectedProduct: null,
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    setSelectedProduct: (state, action) => {
+      state.selectedProduct = action.payload;
+    },
+  },
+});
+
+export const { setSelectedProduct } = productsSlice.actions;
+
+export default productsSlice.reducer;
+
+export const selectAllProducts = (state) => state.products.products;
+export const selectProductStatus = (state) => state.products.status;
+export const selectProductError = (state) => state.products.error;
+export const selectSelectedProduct = (state) => state.products.selectedProduct;
